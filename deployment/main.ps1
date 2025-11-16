0a1d58dcc20bde9fd9ba6e436e5c5f4521fc2439
Jeffkw213 <jeffkwan10@gmail.com>
2025-11-16 00:10:02 -0500

    reading powershell

--- deployment/main.ps1
+++ deployment/main.ps1
@@ -1,3 +1,11 @@
 #!/bin/bash
 # This script prints a greeting message
+# Parameter help description
+param(
+[Parameter(Mandatory=$true)]
+[String] $ParameterName
+)
 Write-Host "Hello, World!"
+Write-Host "This is the deployment script for the CI/CD course"
+Write-Host "Passing Param is $ParameterName"
+Write-host "Secret is $env:SECRET"
